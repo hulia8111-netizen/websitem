d8f6e712dd39ac6bd3ae685fe4fa3caac5dbfcae
Hulya <hulia8111@gmail.com>
2026-08-30 10:23:52 +0300

    Gizlilik politikasi guclendirildi (gelistirici kimligi, hesap silme, Play ibaresi) + uygulama ici link v207

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    SÃ¼rÃ¼m deÄŸiÅŸince CACHE adÄ±nÄ± artÄ±r ki eski dosyalar temizlensin.
    ============================================================ */
 
-const CACHE = "isigini-bul-v206";
+const CACHE = "isigini-bul-v207";
 const KABUK = [
   ".",
   "index.html",
